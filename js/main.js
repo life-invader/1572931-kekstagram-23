2793cbe67112baa6959caf2bce57f76ef82744db
Alex Leontyev <orientstar96@gmail.com>
2021-06-17 16:49:11 +0500

    wraps the code for rendering images into a function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-import './thumbnails-rendering.js';
+import {renderPhotos} from './thumbnails-rendering.js';
+import {createPhotos} from './util.js';
+
+const PHOTOS = new Array(26).fill(null).map(createPhotos);
+renderPhotos(PHOTOS);
--- js/thumbnails-rendering.js
+++ js/thumbnails-rendering.js
@@ -1,18 +1,21 @@
-import {createPhotos} from './util.js';
-
-const photoFragment = document.createDocumentFragment();
 const photoTemplate = document.querySelector('#picture').content;
 const photoField = document.querySelector('.pictures');
 
-const TEST_ARRAY = new Array(26).fill(null).map(createPhotos);
+const renderPhotos = (photos) => {
+  const photoFragment = document.createDocumentFragment();
 
-TEST_ARRAY.forEach(({comments, likes, url}) => {
-  const photoElement = photoTemplate.cloneNode(true);
+  photos.forEach(({comments, likes, url}) => {
+    const photoElement = photoTemplate.cloneNode(true);
 
-  photoElement.querySelector('.picture__img').src = url;
-  photoElement.querySelector('.picture__likes').textContent = likes;
-  photoElement.querySelector('.picture__comments').textContent = comments.length;
+    photoElement.querySelector('.picture__img').src = url;
+    photoElement.querySelector('.picture__likes').textContent = likes;
+    photoElement.querySelector('.picture__comments').textContent = comments.length;
 
-  photoFragment.appendChild(photoElement);
+    photoFragment.appendChild(photoElement);
+  });
+
   photoField.appendChild(photoFragment);
-});
+
+};
+
+export {renderPhotos};
